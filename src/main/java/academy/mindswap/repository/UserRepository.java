6cf64a8e9ef4a2be6a0cc20bf354fa50f0cc7ea1
André Sousa <andre.castro.sousa@gmail.com>
2023-08-14 12:15:01 +0000

    add: auth validation for integration tests

--- src/main/java/academy/mindswap/repository/UserRepository.java
+++ src/main/java/academy/mindswap/repository/UserRepository.java
@@ -9,4 +9,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<User> {
 
+    public User findByEmail(String email) {
+        return find("email", email).firstResult();
+    }
 }
